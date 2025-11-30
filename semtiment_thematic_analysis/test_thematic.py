e1dbdee8179b52e3ee6e320687b19695403245f7
Bnunush <binetibebenunush@gmail.com>
2025-11-30 10:36:46 +0300

    Thematic analysis script and tests

---
+++ semtiment_thematic_analysis/test_thematic.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import pytest
+
+def test_themes():
+    df = pd.read_csv(r'C:\Users\hp\Pictures\fintech-review-analytics\fintech-review-analytics-week2\Data\thematicanalyzed_reviews.csv')
+    assert 'themes' in df.columns
+    # Check at least 2 themes on average
+    avg_themes = df['themes'].apply(lambda x: len(x.split(',')) if x else 0).mean()
+    assert avg_themes >= 2
+    print(f"average themes:{avg_themes}")
